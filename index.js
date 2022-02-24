1be4ae5b24f54336c93e5e9a945b27a375304865
WibuSyariah <mspa43@gmail.com>
2022-02-24 16:09:35 +0700

    mascot

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+let nama = ''
+let deposito = 0
+let coin = deposito / 1000
 
+
+
+var coinFlipButton = document.getElementById('coinFlipButton');
+var outcome = document.querySelector('.outcome');
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * (2 - 1 + 1)) + 1
+}
+
+coinFlipButton.addEventListener('click', function() {
+  var randomNumber = getRandomNumber();
+  outcome.textContent = '';
+  outcome.classList.toggle('flip');
+  outcome.classList.add('toss');
+  
+  // Waits 3sec to display flip result
+  setTimeout(function() {
+    if (randomNumber == 1) {
+      outcome.textContent = 'heads';
+    } else if (randomNumber == 2) {
+      outcome.textContent = 'tails';
+    }
+    outcome.classList.remove('toss');
+  }, 800);
+    
+});
